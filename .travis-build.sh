b005993273ed65522e3dd20316b4ac3f92dfa4ba
Michael Ernst <mernst@cs.washington.edu>
2018-01-06 08:59:59 -0800

    Try `git clone` twice, in case of network lossage

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,12 +5,12 @@ ROOT=$TRAVIS_BUILD_DIR/..
 set -e
 
 ## Build Checker Framework
-(cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+(cd $ROOT && git clone https://github.com/typetools/checker-framework.git) || (cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
 # This also builds annotation-tools and jsr308-langtools
 (cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
 export CHECKERFRAMEWORK=$ROOT/checker-framework
 
 ## Obtain plume-lib
-(cd $ROOT && git clone https://github.com/mernst/plume-lib.git)
+(cd $ROOT && git clone https://github.com/mernst/plume-lib.git) || (cd $ROOT && git clone https://github.com/mernst/plume-lib.git)
 
 make -C $ROOT/plume-lib/java check-types
